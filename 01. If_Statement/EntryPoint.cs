87cb570a1ae6be32141818269f1bbfab0fd8ce17
Lockes <prescott.lawani@gmail.com>
2019-10-08 22:15:06 -0500

    Add project files.

---
+++ 01. If_Statement/EntryPoint.cs
@@ -0,0 +1,108 @@
+﻿using System;
+class EntryPoint
+{
+    static void Main()
+    {
+        //Finish this. 
+        /*
+         * 
+         * 
+        int firstNumber = 4;
+        int secondNumber = 5;
+
+
+        if (secondNumber < firstNumber)
+        {
+        Console.WriteLine("4 is less than 5");
+        }
+        else
+        {
+        Console.WriteLine("4 is bigger than 5");
+        }
+        Console.WriteLine("I am after the IF statement");
+         */
+
+
+        string tempUserName = "";
+        string tempPassword = "";
+        string userName = "";
+        string password = "";
+
+        Console.WriteLine("Enter a username. It must be at least 3 characters:");
+        tempUserName = Console.ReadLine();
+        if(tempUserName.Length >= 3) 
+        { 
+            userName = tempUserName; 
+        }
+        else
+        {
+            userName = "defaultUser";
+        }
+        Console.WriteLine("Enter a password. It must be at least 8 characters, have at least one capital letter, one number:");
+        tempPassword = Console.ReadLine();
+
+        Console.WriteLine();
+
+        //something here is wrong that I can't fix: 
+        //Gotta figure out how to search through all the characters in order to find
+        //a specific character within each one
+
+        bool realPasswordNumber = false;
+        bool realPasswordCase = false;
+
+        for (int i = 0; i < tempPassword.ToString()[tempPassword.ToString().Length]; i++)
+        {
+            if((int)tempPassword[i] >= 48 && (int)tempPassword[i] <= 57)
+        {
+                 realPasswordNumber = true;
+        }
+            else
+            {
+                 realPasswordNumber = false;
+            }
+        }
+
+        for (int j = 0; j < tempPassword.ToString()[tempPassword.ToString().Length]; j++)
+        {
+            if ((int)tempPassword[j] >= 65 && (int)tempPassword[j] <= 90)
+            {
+                realPasswordCase = true;
+            }
+            else
+            {
+                realPasswordCase = false;
+            }
+        }
+
+        //bool realPasswordNumber = (int)tempPassword[tempPassword.Length - 1] >= 48 && (int)tempPassword[tempPassword.Length - 1] <= 57;
+        //bool realPasswordCase = (int)tempPassword[tempPassword.Length - 1] >= 65 && (int)tempPassword[tempPassword.Length - 1] <= 90;
+
+        if (tempPassword.Length >= 8)
+        {
+            if(realPasswordNumber && realPasswordCase)
+            {
+                password = tempPassword;
+            }
+            else if (!realPasswordCase)
+            {
+                password = "defaultpassword";
+            }
+            else if (!realPasswordNumber)
+            {
+                password = "defaultpassword";
+            }
+          
+        }
+        else if (tempPassword.Length < 8)
+        {
+            password = "defaultpassword";
+        }
+        Console.WriteLine("Your new Username is: " + userName);
+        Console.WriteLine("Your new Password is: " + password);
+        //The symbol code is a little difficult, so we'll hold onto it for now
+        //bool realPasswordSymbol = (int)tempPassword[tempPassword.Length] 
+
+
+    }
+}
+
---
+++ 02. IfElseStatement/EntryPoint.cs
@@ -0,0 +1,81 @@
+﻿using System;
+class EntryPoint
+{
+    static void Main()
+    {
+
+        //Read the WriteLine codes to see what this code is trying to perform
+
+        int givenNum = 0;
+        Console.Write("Give me a number. I will see if it is odd or even: ");
+        //Remember to use the Convert.ToInt32 to read what the user types and convert it into an integer
+        givenNum = Convert.ToInt32(Console.ReadLine());
+        int modulousNum = givenNum % 2;
+
+        if(modulousNum == 0)
+        {
+            Console.WriteLine(givenNum + " is an even number.");
+        }
+        else if(modulousNum == 1)
+        {
+            Console.WriteLine(givenNum + " is an odd number.");
+        }
+
+        Console.WriteLine();
+
+        int givenNum1 = 0;
+        Console.Write("Give me another number, I will see if I can divide it\n" +
+            "by 2, 3, or 4: ");
+        givenNum1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        int byTwo = givenNum1 % 2;
+        if (byTwo == 0) { Console.WriteLine(givenNum1 + " can be divided by two."); }
+        else if (byTwo != 0) { Console.WriteLine(givenNum1 + " cannot be divided by two."); }
+
+        int byThree = givenNum1 % 3;
+        if (byThree == 0) { Console.WriteLine(givenNum1 + " can be divided by three."); }
+        else if (byThree != 0) { Console.WriteLine(givenNum1 + " cannot be divided by three."); }
+
+        int byFour = givenNum1 % 4;
+        if (byFour == 0) { Console.WriteLine(givenNum1 + " can be divided by four."); }
+        else if (byFour != 0) { Console.WriteLine(givenNum1 + " cannot be divided by four."); }
+
+        Console.WriteLine();
+
+        Console.Write("Another number please, I'll try to see\n" +
+            "if I can divide it " +
+            "by 2, 3, or 4, all at once: ");
+        int givenNum2 = Convert.ToInt32(Console.ReadLine());
+        if (givenNum2 % 2 == 0 && givenNum2 % 3 == 0 && givenNum2 % 4 == 0)
+        {
+            Console.WriteLine($"The number, {givenNum2} is divisible by 2,3, and 4");
+        }
+        else
+        {
+            Console.WriteLine($"I'm sorry, but {givenNum2} cannot be divided by all 3 numbers");
+        }
+
+
+        Console.WriteLine();
+
+        //write an expression that will check if the 3rd digit of an int is 3
+        //To do this you have to convert to a string
+        //This is probably what you were trying to figure in the if statement project
+        Console.Write("Give me a number, I will check\n" +
+            "if it has a 3 as the 3rd number: ");
+        int givenNum3 = Convert.ToInt32(Console.ReadLine());
+
+        if(givenNum3.ToString()[givenNum3.ToString().Length - 3] == '3')
+        {
+            Console.WriteLine($"Yes, the number {givenNum3} does have a 3 for the 3rd number!");
+        }
+        else
+        {
+            Console.WriteLine($"No, the number {givenNum3} does not have a 3 as the 3rd number!" );
+        }
+        Console.WriteLine();
+    }
+}
+
